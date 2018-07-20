84218abc85924633415320ecb8ab52ce05406d44
Ionut Tutu <ittutu@gmail.com>
2018-07-20 17:19:21 +0100

    Add the ABP-OTS page

--- Website/js/extra.js
+++ Website/js/extra.js
@@ -7,43 +7,12 @@
       scrollTop: $(divId).offset().top - 54
     }, 500);
   });
+
+  // Enable source-code highlighting
+  $(document).ready(function() {
+    $('pre code').each(function(i, block) {
+      hljs.highlightBlock(block);
+    });
+  });
   
 })(jQuery); // End of use strict
-
-// Enable source-code highlighting
-
-var entityMap = {
-  "&": "&amp;",
-  "<": "&lt;",
-  ">": "&gt;",
-  '"': '&quot;',
-  "'": '&#39;',
-  "/": '&#x2F;'
-};
-
-function escapeHtml(string) {
-  return String(string).replace(/[&<>"'\/]/g, function (s) {
-    return entityMap[s];
-  });
-}
-
-//document.addEventListener("DOMContentLoaded", init, false);
-
-window.onload = function init() 
-{
-  var codeblock = document.querySelectorAll("pre code");
-  
-  if(codeblock.length) 
-  {
-    for(var i=0, len=codeblock.length; i<len; i++) 
-    {
-      var dom = codeblock[i];
-      var html = dom.innerHTML;
-      html = escapeHtml(html);
-      dom.innerHTML = html;
-    }
-  }
-}
-
-
-hljs.initHighlightingOnLoad();
